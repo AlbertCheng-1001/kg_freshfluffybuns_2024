203fd5165cf66b84cc8f8561ca5bb7fe0f59f7fd
Albert Cheng <itzalbertcheng@gmail.com>
2021-05-02 14:06:37 -0400

    Renamed Dictionary

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-function number_to_words(number){
-    dict = {0:"zero", 1:"One", 2:"Two", 3:"Three", 4:"Four", 5:"Five", 
+DIGIT_NAME = {0:"zero", 1:"One", 2:"Two", 3:"Three", 4:"Four", 5:"Five", 
         6:"Six", 7:"Seven", 8:"Eight", 9:"Nine"};
-    
+
+function number_to_words(number){
+
     words = "";
 
     while (number > 0){
         last_digit = number % 10;
         number = Math.floor(number/10);
-        words = dict[last_digit] + words;
+        words = DIGIT_NAME[last_digit] + words;
     }
     return words;
 
